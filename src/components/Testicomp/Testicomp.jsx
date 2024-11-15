4413bc5b15a74bae4599afb44cef2b230a92111a
MH-Kniloy <web.kniloy@gmail.com>
2024-11-16 01:30:45 +0600

    Testimonial responsive 768px

--- src/components/Testicomp/Testicomp.jsx
+++ src/components/Testicomp/Testicomp.jsx
@@ -6,10 +6,10 @@ const Testicomp = () => {
   return (
     <>
       <div className="bg-white rounded-md p-[40px]">
-        <h3 className="text-orange font-bold font-heading text-[24px] mb-2">
+        <h3 className="text-orange font-bold font-heading md:text-[24px] text-xl mb-2">
           Fantastic service!
         </h3>
-        <p className="text-[#464558] leading-[145%] text-justify mb-7">
+        <p className="text-[#464558] leading-[145%] text-justify md:mb-7 mb-4 text-[14px] md:text-base">
           I purchased a phone from an e-commerce site, and this courier service
           provider assisted me in getting it delivered to my home. I received my
           phone within one day, and I was really satisfied with their service
@@ -19,15 +19,15 @@ const Testicomp = () => {
           center support and key account managers. They come highly recommended.
           Confidently say that they are really reliable.
         </p>
-        <div className='flex justify-between'>
-          <div className="flex gap-2">
-            <FaStar className="text-orange text-[20px]" />
-            <FaStar className="text-orange text-[20px]" />
-            <FaStar className="text-orange text-[20px]" />
-            <FaStar className="text-orange text-[20px]" />
-            <FaStar className="text-orange text-[20px]" />
+        <div className='md:flex justify-between'>
+          <div className="flex gap-2 mb-4 md:mb-0">
+            <FaStar className="text-orange md:text-[20px]" />
+            <FaStar className="text-orange md:text-[20px]" />
+            <FaStar className="text-orange md:text-[20px]" />
+            <FaStar className="text-orange md:text-[20px]" />
+            <FaStar className="text-orange md:text-[20px]" />
           </div>
-          <div className='flex items-center'>
+          <div className='flex items-center justify-end'>
             <div>
               <h4 className="text-[#464558] font-bold text-end">Yves Tanguy</h4>
               <h5 className="text-[#464558]">Chief Executive, DLF</h5>
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -7,19 +7,19 @@ import Reactslick from '../Reactslick/Reactslick';
 const Testimonial = () => {
   return (
     <>
-      <section className="pb-[270px]">
+      <section className="md:pb-[270px] pb-[80px]">
         <Container>
-          <div className='flex justify-center gap-10 pb-[90px]'>
-            <img src={Quote} alt="" />
-            <div className='flex flex-col justify-end pe-[150px]'>
-              <h3 className='text-[32px] text-orange font-heading font-bold text-center'>TESTIMONIAL</h3>
-              <p className='text-[#11111d] text-[40px] font-bold'>Our Awesome Clients</p>
+          <div className='flex justify-center md:gap-10 gap-6 md:pb-[90px] pb-[80px] me-4 md:me-0'>
+           <img className='w-[50px] md:w-auto' src={Quote} alt="" />
+            <div className='md:flex flex-col justify-center md:pe-[150px]'>
+              <h3 className='md:text-[32px] text-2xl text-orange font-heading font-bold text-center md:pb-4 '>TESTIMONIAL</h3>
+              <p className='text-[#11111d] md:text-[40px] text-xl font-bold'>Our Awesome Clients</p>
             </div>
           </div>
           
              <Reactslick>
 
-          <div className='w-[870px] mx-auto'>
+          <div className='md:w-[870px] md:mx-auto mx-4'>
             <Testicomp />
           </div>
              </Reactslick>
